c710ba27d126e665ab94cc321e25d76f371095fd
Alexander Bolshakov <81434888+Paparadoxx@users.noreply.github.com>
2024-03-02 20:29:32 +0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,82 @@
+slow-deps [![JavaScript Style Guide](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com/) [![No Maintenance Intended](http://unmaintained.tech/badge.svg)](http://unmaintained.tech/)
+====
+
+CLI to measure which dependencies in a project are the slowest to `npm install`.
+
+Usage
+----
+
+    npm install -g slow-deps
+    slow-deps
+
+Example output
+----
+
+```
+Analyzing 7 dependencies...
+[====================] 100% 0.0s
+--------------------------------------------------
+| Dependency           | Time  | Size   | # Deps |
+--------------------------------------------------
+| browserify           | 22.2s | 14 MB  | 94     |
+| jshint               | 7.9s  | 4.6 MB | 26     |
+| istanbul             | 7s    | 5.1 MB | 27     |
+| promises-aplus-tests | 5.4s  | 2.4 MB | 24     |
+| uglify-js            | 4.4s  | 1.7 MB | 18     |
+| mocha                | 4.3s  | 924 KB | 16     |
+| immediate            | 1.3s  | 24 KB  | 0      |
+--------------------------------------------------
+Total time (non-deduped): 52.6s
+Total size (non-deduped): 29 MB
+```
+
+Details
+----
+
+
+Run this in a directory with a `package.json`, and it will take all the `dependencies`, 
+`devDependencies`, and `optionalDependencies`, then `npm install` each one in a temporary 
+directory with a temporary cache, then measure the install times. Each dependency is then listed 
+from slowest to fastest.
+
+In addition to the install time, the size on disk and the total number of transitive dependencies are also printed.
+
+Note that this doesn't measure the cost of npm de-duplicating the sub-dependencies; 
+it only measures the time for installing each dependency independently. So the "total time" and "total size"
+are both just rough estimates, as are the per-package times, because you may have packages with many common
+dependencies that would therefore get de-duped.
+
+It's also worth noting that because this tool creates a temporary npm cache while it works,
+you'll be seeing the _uncached_ time for each package. Therefore your actual `npm install` should run a bit faster,
+assuming everything is cached.
+
+Options
+----
+
+```
+Usage: slow-deps [options]
+
+Options:
+  --production, --prod  Skip devDependencies                           [boolean]
+  --no-optional         Skip optionalDependencies                      [boolean]
+  --no-shrinkwrap       Ignore npm-shrinkwrap.json                     [boolean]
+  -h, --help            Show help                                      [boolean]
+
+Examples:
+  slow-deps                             measure all deps in the current project
+  slow-deps --production --no-optional  skip both optional and dev dependencies
+```
+
+Credits
+---
+
+Huge shout-out to [Michael Hart](https://twitter.com/hichaelmart) for the inspiration for this tool!
+
+Also a big thanks to [all contributors](https://github.com/nolanlawson/slow-deps/graphs/contributors)!
+
+Related
+----
+
+- [pkgfiles](https://www.npmjs.com/package/pkgfiles) - see your project's npm package size before publishing
+- ["files" in package.json](https://docs.npmjs.com/files/package.json#files)
+- ["bundledDependencies" in package.json](https://docs.npmjs.com/files/package.json#bundleddependencies)
---
+++ index.js
@@ -0,0 +1,222 @@
+#!/usr/bin/env node
+
+'use strict'
+
+if (!global.Promise) {
+  global.Promise = require('lie')
+}
+var temp = require('temp')
+var fs = require('fs')
+var exec = require('child-process-promise').exec
+var denodeify = require('denodeify')
+var shellEscape = require('any-shell-escape')
+var extend = require('js-extend').extend
+var mkdir = denodeify(temp.mkdir)
+var readFile = denodeify(fs.readFile)
+var readdir = denodeify(fs.readdir)
+var stat = denodeify(fs.stat)
+var writeFile = denodeify(fs.writeFile)
+var appendFile = denodeify(fs.appendFile)
+var path = require('path')
+var now = require('performance-now')
+var ProgressBar = require('progress')
+var getFolderSize = denodeify(require('get-folder-size'))
+var prettierBytes = require('prettier-bytes')
+var prettyMs = require('pretty-ms')
+var tablify = require('tablify').tablify
+var sum = require('math-sum')
+var ncp = denodeify(require('ncp'))
+var yargs = require('yargs')
+temp.track()
+
+var argv = yargs
+  .usage('Usage: $0 [options]')
+
+  .boolean('production')
+  .describe('production', 'Skip devDependencies')
+  .alias('production', 'prod')
+
+  .boolean('no-optional')
+  .describe('no-optional', 'Skip optionalDependencies')
+
+  .boolean('no-shrinkwrap')
+  .describe('no-shrinkwrap', 'Ignore npm-shrinkwrap.json')
+
+  .example('$0', 'measure all deps in the current project')
+  .example('$0 --production --no-optional', 'skip both optional and dev dependencies')
+
+  .help('help')
+  .alias('h', 'help')
+  .argv
+
+function formatPlural (num, strSingular, strPlural) {
+  return num + ' ' + (num === 1 ? strSingular : strPlural)
+}
+
+function getDeps () {
+  return readFile('package.json', 'utf8').then(function (str) {
+    var pkgJson = JSON.parse(str)
+    var deps = extend({}, pkgJson.dependencies)
+    var devSkipped = 0
+    var optionalSkipped = 0
+    // Include devDependencies only if --production is absent
+    if (!argv.production) {
+      extend(deps, pkgJson.devDependencies)
+    } else {
+      devSkipped = Object.keys(pkgJson.devDependencies || {}).length
+    }
+    // Include optionalDependencies only if --no-optional argument is absent.
+    // Note that yargs parses "--no-*" deps in a special way, hence this `!== false` check.
+    if (argv.optional !== false) {
+      extend(deps, pkgJson.optionalDependencies)
+    } else {
+      optionalSkipped = Object.keys(pkgJson.optionalDependencies || {}).length
+    }
+    var startMessage = 'Analyzing ' + Object.keys(deps).length + ' dependencies'
+    if (devSkipped && optionalSkipped) {
+      startMessage += ' (skipping ' + formatPlural(devSkipped, 'devDependency', 'devDependencies') +
+        ' and ' + formatPlural(optionalSkipped, 'optionalDependency', 'optionalDependencies') + ')'
+    } else if (devSkipped) {
+      startMessage += ' (skipping ' + formatPlural(devSkipped, 'devDependency', 'devDependencies') + ')'
+    } else if (optionalSkipped) {
+      startMessage += ' (skipping ' + formatPlural(optionalSkipped, 'optionalDependency', 'optionalDependencies') + ')'
+    }
+    startMessage += '...'
+    console.log(startMessage)
+    return deps
+  }).catch(function () {
+    throw new Error('No package.json in the current directory.')
+  })
+}
+
+function getShrinkwrapDeps () {
+  return stat('npm-shrinkwrap.json').then(function (file) {
+    if (argv.shrinkwrap !== false && file.isFile()) {
+      return readFile('npm-shrinkwrap.json', 'utf8').then(function (str) {
+        return JSON.parse(str).dependencies || {}
+      })
+    }
+    return {}
+  }).catch(function () {
+    return {}
+  })
+}
+
+function createEmptyNpmrc (dir) {
+  return writeFile(path.join(dir, '.npmrc'), '', 'utf8')
+}
+
+function setupNpmrc (toDir) {
+  // copy .npmrc from current directory if possible
+  return stat('.npmrc').then(function (file) {
+    if (file.isFile()) {
+      return ncp('.npmrc', path.join(toDir, '.npmrc'))
+    }
+    return createEmptyNpmrc(toDir)
+  }).catch(function () {
+    return createEmptyNpmrc(toDir)
+  })
+}
+
+function createNpmShrinkwrap (dir, dep) {
+  return writeFile(path.join(dir, 'npm-shrinkwrap.json'), JSON.stringify(dep), 'utf8')
+}
+
+function createPackageJson (dir, dep, version) {
+  var content = '{ "dependencies": { "' + dep + '": "' + version + '" } }'
+  return writeFile(path.join(dir, 'package.json'), content, 'utf8')
+}
+
+function doNpmInstalls (deps, shrinkwrapDeps) {
+  var promise = Promise.resolve()
+  var bar = new ProgressBar('[:bar] :percent :etas', {
+    total: Object.keys(deps).length,
+    width: 20
+  })
+  var times = []
+
+  function install (dep, version, dir) {
+    var cache = path.join(dir, '.cache')
+    var nodeModules = path.join(dir, 'node_modules')
+
+    return setupNpmrc(dir).then(function () {
+      // set the cache to a local cache directory
+      return appendFile(path.join(dir, '.npmrc'), '\ncache=' + cache, 'utf8')
+    }).then(function () {
+      if (!shrinkwrapDeps[dep]) {
+        return
+      }
+      return createNpmShrinkwrap(dir, shrinkwrapDeps[dep])
+    }).then(function () {
+      return createPackageJson(dir, dep, version)
+    }).then(function () {
+      var start = now()
+      return exec(shellEscape([ 'npm', 'install' ]), {
+        cwd: dir,
+        env: process.env
+      }).then(function () {
+        var totalTime = now() - start
+        return getFolderSize(nodeModules).then(function (size) {
+          return readdir(nodeModules).then(function (subDeps) {
+            times.push({
+              time: totalTime,
+              size: size,
+              dep: dep,
+              subDeps: subDeps.length - 1
+            })
+            bar.tick()
+          })
+        })
+      })
+    })
+  }
+
+  Object.keys(deps).forEach(function (dep) {
+    var version = deps[dep]
+    promise = promise.then(function () {
+      return mkdir('')
+    }).then(function (dir) {
+      return install(dep, version, dir)
+    })
+  })
+  return promise.then(function () {
+    return report(times)
+  })
+}
+
+function report (times) {
+  times = times.sort(function (a, b) {
+    return b.time - a.time
+  })
+  var header = ['Dependency', 'Time', 'Size', '# Deps']
+  var table = [header].concat(times.map(function (time) {
+    return [
+      time.dep,
+      prettyMs(time.time),
+      prettierBytes(time.size),
+      time.subDeps
+    ]
+  }))
+  console.log(tablify(table, {
+    show_index: false,
+    has_header: true
+  }))
+  console.log('Total time (non-deduped): ' + prettyMs(sum(times.map(function (time) {
+    return time.time
+  }))))
+  console.log('Total size (non-deduped): ' + prettierBytes(sum(times.map(function (time) {
+    return time.size
+  }))))
+}
+
+Promise.all([getDeps(), getShrinkwrapDeps()]).then(function (results) {
+  var deps = results[0]
+  var shrinkwrap = results[1]
+  return doNpmInstalls(deps, shrinkwrap)
+}).then(function () {
+  process.exit(0)
+}).catch(function (err) {
+  console.error(err)
+  console.error(err.stack)
+  process.exit(1)
+})
